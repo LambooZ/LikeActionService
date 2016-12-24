71e8e517c9ad63fd33509ae65099b5cc62ea4462
LambooZ <627263110@qq.com>
2016-12-25 02:09:50 +0800

    1

--- service/serviceImpl/LikeActionServiceImpl.java
+++ service/serviceImpl/LikeActionServiceImpl.java
@@ -1,44 +1,37 @@
 package cn.edu.bjtu.weibo.service.Impl;
 
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import cn.edu.bjtu.weibo.dao.CommentDAO;
 import cn.edu.bjtu.weibo.dao.UserDAO;
 import cn.edu.bjtu.weibo.dao.WeiboDAO;
-import cn.edu.bjtu.weibo.dao.Impl.CommentDAOImpl;
-import cn.edu.bjtu.weibo.dao.Impl.UserDAOImpl;
-import cn.edu.bjtu.weibo.dao.Impl.WeiboDAOImpl;
 import cn.edu.bjtu.weibo.service.LikeActionService;
 
+@Service("LikeActionService")
 public class LikeActionServiceImpl implements LikeActionService{
+	@Autowired
+	private UserDAO Udao;
+	@Autowired
+	private CommentDAO Cdao;
+	@Autowired
+	private WeiboDAO Wdao;	
 	
-	@Override
 	public boolean LikeWeiboOrCommentAction(String userId, String weiboOrCommentId) {
-		UserDAO Udao = new UserDAOImpl();
-		CommentDAO Cdao = new CommentDAOImpl();
-		WeiboDAO Wdao = new WeiboDAOImpl();	
-		int pageIndex = 0;
-		int pagePerNumber = 0;
 		
-		if (Cdao.getLikeList(weiboOrCommentId) != null) {
-			List<String> Clist = Cdao.getLikeList(weiboOrCommentId);
-			if(Clist.contains(userId)) {
-				Cdao.deleteLikeUser(weiboOrCommentId, userId);
-			}else {
-				Cdao.insertLikeUser(weiboOrCommentId, userId);
-			}
+		if(weiboOrCommentId.startsWith("w")){
+			Wdao.updateLikeList(weiboOrCommentId, userId);
+			Wdao.updateLikeNmuber(weiboOrCommentId);
+			Udao.insertLikeWeibo(userId, weiboOrCommentId);
+			//String Owner = Wdao.getOwner(weiboOrCommentId);
+			return true;
+		}else if(weiboOrCommentId.startsWith("C")) {
+			Cdao.updateLikeList(weiboOrCommentId, userId);
+			Cdao.updateLikeNumber();
+			//String Owner = Cdao.getOwner(weiboOrCommentId);
+			return true;
+		}else {
+			return false;
 		}
-		if (Wdao.getLikeList(weiboOrCommentId, pageIndex, pagePerNumber) != null) {
-			List<String> Wlist = Wdao.getLikeList(weiboOrCommentId, pageIndex, pagePerNumber);
-			if(Wlist.contains(userId)) {
-				Wdao.deleteLikeUser(weiboOrCommentId, userId);
-				Udao.deleteLikeWeibo(userId,weiboOrCommentId);//从我赞过的删除
-			}else {
-				Wdao.insertLikeUser(weiboOrCommentId, userId);
-				Udao.insertLikeWeibo(userId,weiboOrCommentId);//加入我赞过的
-			}
-		}
-		
-		return true;
 	}
 }
