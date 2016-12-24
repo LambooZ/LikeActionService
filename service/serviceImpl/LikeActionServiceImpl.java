1d88d3524154a183d585c35555d91647fdde3676
LambooZ <627263110@qq.com>
2016-12-25 02:37:04 +0800

    1

--- service/serviceImpl/LikeActionServiceImpl.java
+++ service/serviceImpl/LikeActionServiceImpl.java
@@ -7,6 +7,7 @@ import cn.edu.bjtu.weibo.dao.CommentDAO;
 import cn.edu.bjtu.weibo.dao.UserDAO;
 import cn.edu.bjtu.weibo.dao.WeiboDAO;
 import cn.edu.bjtu.weibo.service.LikeActionService;
+import cn.edu.bjtu.weibo.service.MessageToMeService;
 
 @Service("LikeActionService")
 public class LikeActionServiceImpl implements LikeActionService{
@@ -16,19 +17,26 @@ public class LikeActionServiceImpl implements LikeActionService{
 	private CommentDAO Cdao;
 	@Autowired
 	private WeiboDAO Wdao;	
+	@Autowired
+	private MessageToMeService service ;
 	
 	public boolean LikeWeiboOrCommentAction(String userId, String weiboOrCommentId) {
 		
 		if(weiboOrCommentId.startsWith("w")){
-			Wdao.updateLikeList(weiboOrCommentId, userId);
-			Wdao.updateLikeNmuber(weiboOrCommentId);
-			Udao.insertLikeWeibo(userId, weiboOrCommentId);
-			//String Owner = Wdao.getOwner(weiboOrCommentId);
+			Wdao.updateLikeList(weiboOrCommentId, userId);//更新赞的列表
+			Wdao.updateLikeNmuber(weiboOrCommentId);//更新赞的数目
+			Udao.insertLikeWeibo(userId, weiboOrCommentId);//insert和delete应该合并成update？
+			//Udao.deleteLikeWeibo(userId, weiboOrCommentId);
+			
+			String Owner = Wdao.getOwner(weiboOrCommentId);
+			service.likeMyWeiboInform(userId, Owner, weiboOrCommentId);//发消息
 			return true;
-		}else if(weiboOrCommentId.startsWith("C")) {
-			Cdao.updateLikeList(weiboOrCommentId, userId);
-			Cdao.updateLikeNumber();
-			//String Owner = Cdao.getOwner(weiboOrCommentId);
+		}else if(weiboOrCommentId.startsWith("c")) {
+			Cdao.updateLikeList(weiboOrCommentId, userId);//更新赞的列表
+			Cdao.updateLikeNumber();//更新赞的数目
+			
+			String Owner = Cdao.getOwner(weiboOrCommentId);
+			service.likeMyCommentInform(userId, Owner, weiboOrCommentId);//发消息
 			return true;
 		}else {
 			return false;
