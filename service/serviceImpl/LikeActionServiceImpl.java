d09ae725921b4a11897087134039163014edc983
LambooZ <627263110@qq.com>
2016-12-26 22:56:00 +0800

    1

--- service/serviceImpl/LikeActionServiceImpl.java
+++ service/serviceImpl/LikeActionServiceImpl.java
@@ -1,5 +1,7 @@
 package cn.edu.bjtu.weibo.service.Impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,34 +14,50 @@ import cn.edu.bjtu.weibo.service.MessageToMeService;
 @Service("LikeActionService")
 public class LikeActionServiceImpl implements LikeActionService{
 	@Autowired
-	private UserDAO Udao;
+	private UserDAO udao;
 	@Autowired
-	private CommentDAO Cdao;
+	private CommentDAO cdao;
 	@Autowired
-	private WeiboDAO Wdao;	
+	private WeiboDAO wdao;	
 	@Autowired
 	private MessageToMeService service ;
 	
-	public boolean LikeWeiboOrCommentAction(String userId, String weiboOrCommentId) {
+	public boolean LikeWeiboOrCommentAction(String userId, String weiboOrCommentId,int pageIndex, int numberPerPage) {
 		
 		if(weiboOrCommentId.startsWith("w")){
-			Wdao.updateLikeList(weiboOrCommentId, userId);//更新赞的列表
-			Wdao.updateLikeNmuber(weiboOrCommentId);//更新赞的数目
-			Udao.insertLikeWeibo(userId, weiboOrCommentId);//insert和delete应该合并成update？
-			//Udao.deleteLikeWeibo(userId, weiboOrCommentId);
-			
-			String Owner = Wdao.getOwner(weiboOrCommentId);
-			service.likeMyWeiboInform(userId, Owner, weiboOrCommentId);//发消息
+			List<String> WeiboLikeList = wdao.getLikeList(weiboOrCommentId, pageIndex, numberPerPage);
+			String Owner = wdao.getOwner(weiboOrCommentId);
+			if(WeiboLikeList.contains(userId)) {
+				wdao.deleteLikeList(weiboOrCommentId, userId);
+				udao.deleteLikeWeibo(userId, weiboOrCommentId);
+				udao.deleteWeiboLikeMe(userId, Owner, weiboOrCommentId);
+			}else {
+				wdao.insertLikeList(weiboOrCommentId, userId);
+				udao.insertLikeWeibo(userId, weiboOrCommentId);
+				udao.insertWeiboLikeMe(userId, Owner, weiboOrCommentId);
+				service.likeMyWeiboInform(userId, Owner, weiboOrCommentId);//发消息
+			}
 			return true;
 		}else if(weiboOrCommentId.startsWith("c")) {
-			Cdao.updateLikeList(weiboOrCommentId, userId);//更新赞的列表
-			Cdao.updateLikeNumber();//更新赞的数目
-			
-			String Owner = Cdao.getOwner(weiboOrCommentId);
-			service.likeMyCommentInform(userId, Owner, weiboOrCommentId);//发消息
+			List<String> CommentLikeList = cdao.getLikeList(weiboOrCommentId, pageIndex, numberPerPage);
+			String Owner = cdao.getOwner(weiboOrCommentId);
+			if(CommentLikeList.contains(userId)) {
+				cdao.deleteLikeList(weiboOrCommentId, userId);
+				udao.deleteCommentLikeMe(userId, Owner, weiboOrCommentId);
+			}else {
+				cdao.insertLikeList(weiboOrCommentId, userId);
+				udao.insertCommentLikeMe(userId, Owner, weiboOrCommentId);
+				service.likeMyCommentInform(userId, Owner, weiboOrCommentId);
+			}
 			return true;
 		}else {
 			return false;
 		}
+	}
+
+	@Override
+	public boolean LikeWeiboOrCommentAction(String userId, String weiboOrCommentId) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
